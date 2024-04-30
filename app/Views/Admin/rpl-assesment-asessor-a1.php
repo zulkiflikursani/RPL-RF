6f27c18ee2f2d3e9660ba066005d7a053c3f510a
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2024-04-30 15:40:32 +0800

    test update lenovo

--- app/Views/Admin/rpl-assesment-asessor-a1.php
+++ app/Views/Admin/rpl-assesment-asessor-a1.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <!-- test update -->
+    <!-- test update lenovo -->
     <?= $title_meta ?>
     <?= $this->include('partials/rpl-head-css') ?>
     <link href="<?= base_url() ?>/assets/libs/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
